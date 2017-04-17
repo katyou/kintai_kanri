08ae179d235ac23a44607d79c9f5159515c97a01
katyou <ienaga@ijgn.jp>
2017-04-17 15:07:33 +0900

    made correspondence possible　of i18n

--- app/models/work_time.rb
+++ app/models/work_time.rb
@@ -1,11 +1,12 @@
 class WorkTime < ApplicationRecord
   belongs_to :user
 
-  attr_accessor :month
-
   validates :get_month, presence: true
 
   #ユーザー情報を持つWorkTimeをすべて取得する
   scope :get_user, -> (current_user){where(user_id: current_user.id)}
+  #ログインユーザーとは別のユーザーのWorkTimeをすべて取得する
+  scope :get_other_user, -> (user_id){where(user_id: user_id)}
+
 
 end
